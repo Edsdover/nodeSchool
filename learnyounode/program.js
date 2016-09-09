ad0dedacbbe19807d8395ba58a3e3960b6551963
Edwin Dover <edsdover@gmail.com>
2016-09-09 16:21:04 -0700

    finishing javascripting and starting learnyounode

---
+++ learnyounode/program.js
@@ -0,0 +1,18 @@
+
+var fs = require('fs');
+
+
+
+
+// var bufferString = [];
+// fs.readFile(process.argv[2], 'utf8', function (err, data){
+//   if (err) throw err;
+//   var newLines = data.split('\n').length;
+//   console.log(newLines - 1);
+// });
+
+// var total = 0;
+// for (i = 2; i < process.argv.length; i++){
+//   total += Number(process.argv[i]);
+// }
+// console.log(total);
