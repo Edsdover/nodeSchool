00f8d6d8d28e8de097081ca45adf16552d3bd431
Edwin Dover <edsdover@gmail.com>
2016-09-10 13:33:18 -0700

    more work on learnyounode

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,8 +1,34 @@
 
 var fs = require('fs');
+var path=require('path');
+
+// MAKE IT MODLAR
 
 
-
+// FILTERED LS
+// fs.readdir(process.argv[2], function(err, list){
+//   if (err) throw err;
+//   var extensionList = [];
+//   var firstExt = '';
+//
+//   list.forEach(function(file){
+//     extensionList.push(file);
+//   });
+//
+//   extensionList.filter(firstExtension);
+//
+//   function firstExtension(value){
+//     var valExt = value.split('.')[1];
+//     if (firstExt === ''){
+//       firstExt = valExt;
+//       console.log(value);
+//       return value;
+//     } else if (valExt === firstExt){
+//       console.log(value);
+//       return value;
+//     }
+//   }
+// });
 
 // var bufferString = [];
 // fs.readFile(process.argv[2], 'utf8', function (err, data){
